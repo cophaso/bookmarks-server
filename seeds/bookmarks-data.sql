2a688b0e7776ce852f02813fef510f99653c00f7
Crystal Ophaso <crystalo@procore.com>
2019-09-21 14:14:12 -0700

    db fun

---
+++ seeds/bookmarks-data.sql
@@ -0,0 +1,64 @@
+-- insert some test data
+-- Using a multi-row insert statement here
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+(
+   'Thinkful',
+   'https://www.thinkful.com',
+   'Think outside the classroom',
+   5
+),
+(
+   'Google',
+   'https://www.google.com',
+   'Where we find everything else',
+   4
+),
+(
+   'MDN',
+   'https://developer.mozilla.org',
+   'The only place to find web documentation',
+   5
+),
+(
+   'YouTube',
+   'https://www.youtube.com',
+   'Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube.',
+   4
+),
+(
+   'Facebook',
+   'https://www.facebook.com',
+   'Create an account or log into Facebook. Connect with friends, family and other people you know. Share photos and videos, send messages and get updates.',
+   1
+),
+(
+   'Twitter',
+   'https://twitter.com',
+   'From breaking news and entertainment to sports and politics, get the full story with all the live commentary.',
+   3
+),
+(
+   'LinkedIn',
+   'https://www.linkedin.com',
+   '500 million+ members | Manage your professional identity. Build and engage with your professional network. Access knowledge, insights and opportunities.',
+   2
+),
+(
+   'Amazon',
+   'https://www.amazon.com/',
+   'Online shopping from the earths biggest selection of books, magazines, music, DVDs, videos, electronics, computers, software, apparel & accessories, shoes, ...',
+   4
+),
+(
+   'Netflix',
+   'https://www.netflix.com',
+   'Watch Netflix movies & TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more.',
+   5
+),
+(
+   'GMail',
+   'https://www.google.com/gmail/',
+   'Gmail is email that is intuitive, efficient, and useful. 15 GB of storage, less spam, and mobile access.',
+   5
+);
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const bookmarksRouter = require('./bookmarks/bookmarks-router')
 const errorHandler = require('./error-handler');
 const validateBearerToken = require('./validate-bearer-token')
+const BookmarksService = require('./bookmarks-service')
 
 const { NODE_ENV } = require('./config');
 
@@ -25,5 +26,28 @@ app.use(bookmarksRouter);
 app.get('/', (req,res) => {
   res.send('Welcome to the Bookmarks Server!')
 });
+
+app.get('/bookmarks', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  BookmarksService.getAllBookmarks(knexInstance)
+    .then(bookmarks => {
+      res.json(bookmarks)
+    })
+    .catch(next)
+})
+
+app.get('/bookmarks/:bookmarks_id', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  BookmarksService.getById(knexInstance, req.params.article_id)
+    .then(bookmark => {
+      if (!bookmark) {
+        return res.status(404).json({
+          error: { message: `bookmark doesn't exist` }
+        })
+      }
+      res.json(bookmark)
+    })
+    .catch(next)
+})
 
 module.exports = app
