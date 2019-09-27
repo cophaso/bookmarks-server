52aec0bcdb3f51b8614ce35a19027b69aac995cf
Crystal Ophaso <crystalo@procore.com>
2019-09-26 18:58:22 -0700

    fix with solution

--- src/app.js
+++ src/app.js
@@ -1,53 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const bookmarksRouter = require('./bookmarks/bookmarks-router')
-const errorHandler = require('./error-handler');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config.js')
 const validateBearerToken = require('./validate-bearer-token')
-const BookmarksService = require('./bookmarks-service')
+const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
-const { NODE_ENV } = require('./config');
+const app = express()
 
-const app = express();
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
+app.use(validateBearerToken)
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+app.use(bookmarksRouter)
 
-app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
-app.use(errorHandler);
-app.use(validateBearerToken);
-app.use(bookmarksRouter);
-
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('Welcome to the Bookmarks Server!')
-});
-
-app.get('/bookmarks', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  BookmarksService.getAllBookmarks(knexInstance)
-    .then(bookmarks => {
-      res.json(bookmarks)
-    })
-    .catch(next)
 })
 
-app.get('/bookmarks/:bookmarks_id', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  BookmarksService.getById(knexInstance, req.params.article_id)
-    .then(bookmark => {
-      if (!bookmark) {
-        return res.status(404).json({
-          error: { message: `bookmark doesn't exist` }
-        })
-      }
-      res.json(bookmark)
-    })
-    .catch(next)
-})
+app.use(errorHandler)
 
 module.exports = app
